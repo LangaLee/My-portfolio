bcca55733da4f66e9eac06a93382112a2902b8de
LangaLee <langa4749@gmail.com>
2024-03-11 10:06:07 +0000

    updated the intro section

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -17,8 +17,22 @@ const HomePage = () => {
             and software development provides diverse avenues to apply it.
             Coding is not just a solution to problem solving but a power to
             enhance our daily lives when applied correctly. Determination drives
-            to relentlessly pursue solutions until I find the most effective
-            approach to a problem.
+            me to relentlessly pursue solutions until I find the most effective
+            approach to a problem. One of the many beauties of software
+            development is that there is usually more than 1 language to use in
+            designing specific software and in those languages there are more
+            than a few avenues to approach the problem.
+          </p>
+
+          <p>
+            Continuous learning is very crucial as software developers as you
+            can never fully master a language and there are always improvements
+            that can be made in our code. Currently I mainly write code in
+            JavaScript but I am learning C# as I have always loved video games
+            and I would love the ability contribute to their production. Video
+            game development is an example of how much freedom software
+            development gives us and with great creativity we can make great
+            software.
           </p>
         </div>
       </section>
